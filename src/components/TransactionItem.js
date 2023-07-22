dd0fee2db56dac5530f8287c2af98824af3de4d5
kylanalicia <alicia.kimani@student.moringaschool.com>
2023-07-22 10:31:25 +0300

    completed assignment

--- src/components/TransactionItem.js
+++ src/components/TransactionItem.js
@@ -1,24 +1,32 @@
 import React from "react";
 
-function TransactionItem({ transaction,onDeleteTransaction }) {
-    let { date,desription,category,amount } = transaction;
+function TransactionItem({ transaction, onDeleteTransaction }) {
+    let { date, description, category, amount } = transaction;
 
+    // Function to handle deleting a transaction
     function handleTransactionDelete() {
-        fetch(` http://localhost:8000/transactions/${transaction.id}`,{
-            method:"DELETE"
+        // Send a DELETE request to the server to delete the transaction
+        fetch(`http://localhost:8000/transactions/${transaction.id}`, {
+            method: "DELETE"
         })
         .then(response => response.json())
         .then(() => onDeleteTransaction(transaction));
     }
+
     return (
         <tbody>
             <tr>
+                {/* Render the transaction details */}
                 <td>{date}</td>
-                <td>{desription}</td>
+                <td>{description}</td>
                 <td>{category}</td>
                 <td>{amount}</td>
+
+                {/* Render the button to delete the transaction */}
                 <td>
-                    <button style={{color: "blue" }} onClick={handleTransactionDelete}>DELETE</button>
+                    <button style={{ color: "blue" }} onClick={handleTransactionDelete}>
+                        DELETE
+                    </button>
                 </td>
             </tr>
         </tbody>
