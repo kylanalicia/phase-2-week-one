c48819e9485b0f79eb22afaacd858176498d8217
kylanalicia <alicia.kimani@student.moringaschool.com>
2023-07-22 10:25:40 +0300

    completed assignment

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from "react";
 import TransactionItem from "./TransactionItem";
 import SearchTransaction from "./SearchTransaction";
-import AddTransactionForm from "./AddTransactionForm"; 
+import AddTransactionForm from "./AddTransactionForm";
 
 function TransactionList() {
   const [transactions, setTransactions] = useState([]);
   const [searchParam, setSearchParam] = useState("");
 
+  // Fetch the transactions from the server 
   useEffect(() => {
     fetch(`http://localhost:8000/transactions`)
       .then((response) => response.json())
       .then((transactions) => setTransactions(transactions));
   }, []);
 
+  // Function to handle adding a new transaction
   function handleAddTransaction(newTransaction) {
     setTransactions([...transactions, newTransaction]);
   }
 
+  // Function to handle deleting a transaction
   function handleTransactionDelete(deletedTransaction) {
     const updatedTransactions = transactions.filter(
       (transaction) => transaction.id !== deletedTransaction.id
@@ -24,20 +27,23 @@ function TransactionList() {
     setTransactions(updatedTransactions);
   }
 
+  // Filter the transactions based on the search parameter
   const filteredTransactions = transactions.filter((transaction) =>
     transaction.description?.toLowerCase().includes(searchParam.toLowerCase())
   );
 
   return (
     <div>
-      <div>
-        <SearchTransaction
-          searchParam={searchParam}
-          onTransactionSearch={setSearchParam}
-        />
-      </div>
+      {/* Render the search transaction component */}
+      <SearchTransaction
+        searchParam={searchParam}
+        onTransactionSearch={setSearchParam}
+      />
+
+      {/* Render the add transaction form */}
       <AddTransactionForm onAddTransaction={handleAddTransaction} />
 
+      {/* Render the table with transaction items */}
       <table className="ui celled striped padded table">
         <thead className="ui center aligned header">
           <tr>
